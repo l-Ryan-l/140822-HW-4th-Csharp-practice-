ca9f839713bdba79477a299cc30d3a914f2ad8be
Ryan <llryanll@yandex.ru>
2022-08-19 23:28:02 +0300

    task1 solved

--- task1-AtoBPower/Program.cs
+++ task1-AtoBPower/Program.cs
@@ -2,3 +2,25 @@
 // и возводит число A в натуральную степень B.
 // 3, 5 -> 243 (3⁵)
 // 2, 4 -> 16
+
+int Power (int numberA, int numberB)
+{
+int result = 1;
+int index = 1;
+while (index < numberB +1 )
+    {
+        result = Convert.ToInt32(numberA * result);
+        index++;
+    }
+return result;
+}
+
+Console.WriteLine("Введите число A: ");
+int numberA = int.Parse(Console.ReadLine()!);
+
+Console.WriteLine("Введите число B: ");
+int numberB = int.Parse(Console.ReadLine()!);
+
+int result = Power (numberA, numberB);
+Console.WriteLine(result);
+
